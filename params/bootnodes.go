51b6e03ed5c91d0d11ce596eb3ebe0f9ddb62abb
RS Finance Labs <52669170+RS-Finance@users.noreply.github.com>
2021-09-07 07:35:18 -0700

    Update bootnodes.go

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -21,8 +21,13 @@ import "github.com/ethereum/go-ethereum/common"
 // MainnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
 // the main KCC network.
 var MainnetBootnodes = []string{
-	"enode://d16cf20214ae987c0b200252df331633a680866e75fec511ae25573c06585a8ce2d7379526c9ca186501fbcf1af92443a178f7a800049947a178d5a7ba130566@13.230.226.55:30303",  // kcc-mainnet-node-boot-01
-	"enode://0a3e227c2babb0f63e7fbb942f6c1abf00e47dc2e8b970b8c10b6c99dba7ac5eb223a11259eae50d195f88d27d3779a9b7b97618ca2afa5932c7ffdfabc91c31@35.74.148.173:30303",  //kcc-mainnet-node-boot-02
+	"enode://8c5f9c393d84c671608ce8b259a53810629019059944e7ca1ca6fcd2a5842e167bc0115241fb1e6336158f11ce8ac2169338211d3c9a4c9751788642faf7d091@161.35.11.58:30303", // kcc-testnet-node-boot-01
+	"enode://8c5f9c393d84c671608ce8b259a53810629019059944e7ca1ca6fcd2a5842e167bc0115241fb1e6336158f11ce8ac2169338211d3c9a4c9751788642faf7d091@127.0.0.1:30303",
+	
+}
+
+// TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the test network.
+var TestnetBootnodes = []string{
 	"enode://17cdac7246184e3c406e62d6dbd61f8ffcfbf7c4e652695b19f184b6d2d9c0ddc8afa745c80371fbc486462c9eb0ac897f044dbcdcff629839b3621cabc7a265@34.97.13.219:30303",   //kcc-mainnet-node-boot-03
 	"enode://58c66d5d9bdd369fecb8f4d3a081e02f7506e2b950aeeeed65e03ff4cbfd8177eac31c80c9f730b150f92e482828879d44e862ace9013d2f9bd318b42edf4989@34.97.171.225:30303",  //kcc-mainnet-node-boot-04
 	"enode://b36a0aabf8e4e01aeb953d8b87991f164451b4b6ec810e477f633ca7982c5dc34ec025fc4b18e3b89536801388b3950f362bd675d3c20f89c39f43e22d44bdac@35.194.173.113:30303", //kcc-mainnet-node-boot-05
@@ -32,12 +37,6 @@ var MainnetBootnodes = []string{
 	"enode://446d3ab031578915d90b07b49fbc26f7b99a48f670ced4985d53d413bed49e0d332585dd5ee27ad3fe782aa2ccfe0bbd5973d70272fe75e8a75642133de8280c@34.82.155.210:30303",  //kcc-mainnet-node-boot-09
 	"enode://c3b18858f03546f130bb6f0ba677f33f4087acd23bb1da05bc76738c3545afa6c4d69686a16c74f78437c26733140ff984a597532301d0b2752321d00ca70971@35.187.66.121:30303",  //kcc-mainnet-node-boot-10
 	"enode://ef65959234a42fa02572d4ce65805ca58963289df172d54e2b078240bbdfef0cc5040b614b5aa6c72bd6486ad43f392f59b7367fa58ade1f1b5f88b4c3c95b8b@35.234.99.7:30303",    //kcc-mainnet-node-boot-11
-}
-
-// TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the test network.
-var TestnetBootnodes = []string{
-	"enode://8c5f9c393d84c671608ce8b259a53810629019059944e7ca1ca6fcd2a5842e167bc0115241fb1e6336158f11ce8ac2169338211d3c9a4c9751788642faf7d091@161.35.11.58:30303", // kcc-testnet-node-boot-01
-	"enode://8c5f9c393d84c671608ce8b259a53810629019059944e7ca1ca6fcd2a5842e167bc0115241fb1e6336158f11ce8ac2169338211d3c9a4c9751788642faf7d091@127.0.0.1:30303",
 }
 
 var V5Bootnodes []string
